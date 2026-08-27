1bf28c90097181a9b7cee7bba245cc23c2fd8aaa
Ramon Barbosa <ramonbarbosafreitas@gmail.com>
2026-08-26 23:18:20 -0300

    chore: health message changed ok -> tudo na boa

--- handlers.go
+++ handlers.go
@@ -22,7 +22,7 @@ type CreateKeyResponse struct {
 // healthHandler é um simples endpoint de verificação de saúde
 func (a *App) healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(map[string]string{"status": "OK"}); err != nil {
+	if err := json.NewEncoder(w).Encode(map[string]string{"status": "TUDO NA BOA"}); err != nil {
 		log.Printf("Erro ao escrever resposta JSON: %v", err)
 	}
 }
